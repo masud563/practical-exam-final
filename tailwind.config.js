3644d8946b5ab049c454fdb8871afed4b4bef77b
masud563 <sydmasud563@gmail.com>
2022-02-06 03:49:36 +0600

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  
   theme: {
     extend: {},
   },
